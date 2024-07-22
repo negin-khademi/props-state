0d1d73b7c35820203ad3f4649bd1ee5be244ceb8
Negin <negin.khademi@gmail.com>
2024-07-22 14:44:52 +0330

    seprated Header & CoreConcepts from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
 import { CORE_CONCEPTS } from './data';
-import reacrImg from './assets/react-core-concepts.png';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reacrImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>{description} React concepts you will need for almost any app you are going to build!</p>
-    </header>
-  );
-}
-
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import CoreConcept from './components/CoreConcept';
+import Header from './components/Header';
 
 function App() {
   return (
