ba90c9aff98f65804f1dd618d52976e5cbeb5a7d
Negin <negin.khademi@gmail.com>
2024-07-26 01:11:20 +0330

    CSS Styling _ Dynamic Styling

--- src/App.jsx
+++ src/App.jsx
@@ -44,10 +44,18 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleClick('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleClick('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleClick('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleClick('state')}>State</TabButton>
+            <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleClick('components')}>
+              Components
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleClick('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleClick('props')}>
+              Props
+            </TabButton>
+            <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleClick('state')}>
+              State
+            </TabButton>
           </menu>
           {tabContent}
         </section>
